14b1ec0b44ea112fdd8b6f7482063c663be3e778
Alexander Gelshtein <agelshtein1998@gmail.com>
2019-02-28 21:40:10 +0300

    Fractions class
    Class to work with set of fractions

---
+++ src/RationalFracture.java
@@ -0,0 +1,21 @@
+public class RationalFracture {
+
+    private int n, d;
+
+    public void fracture(int nominator, int denominator) {
+        n = nominator;
+        d = denominator;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
